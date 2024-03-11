bcc4376ee11b35ce140e70c273f151e627566dc7
NotNewAnymore <Henryfleischer01@gmail.com>
2024-03-11 16:25:45 -0700

    Added combineSOunds

--- Main.rb
+++ Main.rb
@@ -1,4 +1,4 @@
-require("colorize")
+require("colorize") #External library for printing to console in color.
 $position = 0
 $magnitude = 0
 data = ""
@@ -8,8 +8,8 @@ $waveLengthMult = 1
 def sineWave(data, noise)
   ($rate).times {
     $position += $waveLengthMult
-    $magnitude = (Math.sin(($position + Random.rand(0..noise)) * 0.02 + 1) + 1) * 127
-    $magnitude = $magnitude.round
+    $magnitude = (Math.sin(($position + Random.rand(0..noise)) * 0.02 + 1) + 1) * 127 #Run-on sentance but in code. Figures out the current magnitude from the position, noise, and a pile of constants.
+    $magnitude = $magnitude.round #Turn the magnitude into an int so it can be saved as a char
     displayMagnitude $magnitude
     data += $magnitude.chr()
   }
@@ -19,8 +19,6 @@ end
 def squareWave(data, noise)
   ($rate).times {
     $position += $waveLengthMult
-    #$magnitude = Math.sin(($position + Random.rand(0..noise))).round()
-    #$magnitude = ($magnitude + 1) * 127.round
     if ($position % (240) < 110)
       $magnitude = 255
     else
@@ -36,7 +34,7 @@ def tanWave(data, noise)
   ($rate).times {
     $position += $waveLengthMult
     $magnitude = (Math.tan(($position + Random.rand(0..noise)) * 0.02 + 1) + 1) * 127
-    $magnitude = ($magnitude.round) % 255
+    $magnitude = ($magnitude.round) % 255 #Turn the magnitude into an int so it can be saved as a char, and ensure it's between 0 and 255 without adding too much distortion
     displayMagnitude($magnitude)
     data += $magnitude.chr()
   }
@@ -67,8 +65,27 @@ def displayMagnitude(magnitude)
 end
 
 #Iterate through all arrays in input, add them together, then divide by the number of items in input.
-def combineSounds (input)
-
+def combineSounds(input)
+  datHolder = []
+  input.each { |x|  #Outer loop, iterates through the sound container
+    x.length.times { |i|  #Inner loop, iterates through the sound
+      if datHolder[i].nil?    #If the spot is empty, set it to the value of the current tone
+        datHolder[i] = x[i].ord
+      else                    #If the spot is not empty, add the current sound to the previous sound
+        datHolder[i] += x[i].ord
+      end
+    }
+  }
+  datHolder.length.times{|i|  #Divide everything by the number of sounds, to finish averaging it.
+    datHolder[i] /= input.count
+    datHolder[i] = datHolder[i].chr
+    }
+  datout = ""
+  datHolder.each{|x|  #Display sounds in visualizer
+                 datout += x
+                displayMagnitude(x.ord)
+    }
+  datout              #Return combined sounds
 end
 
 if __FILE__ == $0
@@ -80,23 +97,23 @@ if __FILE__ == $0
 1: Square Wave\n
 2: Tan wave"
     ui = gets().chomp
-    if ui == "0"  #Sine wave
+    if ui == "0" #Sine wave
       puts "What tone multiplier do you want?"
       $waveLengthMult = gets.to_f()
       puts "How much noise do you want?"
-      data = sineWave(data, gets.to_i())
+      data = sineWave("", gets.to_i())
       puts "Generated a sine wave"
     elsif ui == "1" #Square wave
       puts "What tone multiplier do you want?"
       $waveLengthMult = gets.to_f()
       puts "How much noise do you want?"
-      data = squareWave(data, gets.to_i())
+      data = squareWave("", gets.to_i())
       puts "Generated a square wave"
     elsif ui == "2" #Tan wave
       puts "What tone multiplier do you want?"
       $waveLengthMult = gets.to_f()
       puts "How much noise do you want?"
-      data = tanWave(data, gets.to_i())
+      data = tanWave("", gets.to_i())
       puts "Generated a tan wave"
     end
     sounds << data
@@ -105,7 +122,7 @@ if __FILE__ == $0
       continue = true
     end
   end
-
+  data = combineSounds(sounds)
   puts "Import as unsigned 8-bit PCM"
   File.write("Sample", data)
 end
